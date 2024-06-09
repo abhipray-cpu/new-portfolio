0aee778963a79f47adff17cf1235f7aa4c824cb0
abhipray-cpu <dumkaabhipray@gmail.com>
2024-06-09 21:11:20 +0530

    done and ready to be deployed

--- src/components/work/ConnectForm.vue
+++ src/components/work/ConnectForm.vue
@@ -10,14 +10,14 @@
         <form ref="form" class="flex flex-col gap-6" v-else>
             <input type="text"
                 class="w-[90vw] md:w-[30vw] h-[10vh] md:h-[8vh] rounded-md bg-slate-700 border-none focus:outline-none font-inter-normal text-gray-200 px-4 font-normal text-lg md:text-xl"
-                placeholder="Name" v-model="name" name="user_name">
+                placeholder="Name" v-model="name" name="from_name">
+            <input type="email"
+                class="w-[90vw] md:w-[30vw] h-[10vh] md:h-[8vh] rounded-md bg-slate-700 focus:outline-none font-inter-normal text-gray-200 px-4 font-normal text-lg md:text-xl"
+                placeholder="Email" v-model="email" name="from_email">
             <input type="text"
                 class="w-[90vw] md:w-[30vw] h-[10vh] md:h-[8vh] rounded-md bg-slate-700 focus:outline-none font-inter-normal text-gray-200 px-4 font-normal text-lg md:text-xl"
-                placeholder="Email" v-model="email" name="user_email">
-            <input type="text"
-                class="w-[90vw] md:w-[30vw] h-[10vh] md:h-[8vh] rounded-md bg-slate-700 focus:outline-none font-inter-normal text-gray-200 px-4 font-normal text-lg md:text-xl"
-                placeholder="Contact" v-model="contact" name="message">
-            <textarea name="" id=""
+                placeholder="Contact" v-model="contact" name="from_contact">
+            <textarea name="message" id="message"
                 class="w-[90vw] md:w-[30vw] h-[15vh] md:h-[12vh] rounded-md bg-slate-700 focus:outline-none font-inter-normal text-gray-200 px-4 py-2 font-normal text-lg md:text-xl"
                 v-model="message" placeholder="Message"></textarea>
         </form>
@@ -53,7 +53,7 @@ export default {
         const sendEmail = () => {
             if (name.value !== '' && email.value !== '' && contact.value !== '' && message.value != '') {
                 emailjs
-                    .sendForm('service_fph93rk', 'template_88lwdjw', form.value, {
+                    .sendForm('service_fph93rk', 'template_i4z2zke', form.value, {
                         publicKey: 'lmVfpFdCb2s6x_SqC',
                     })
                     .then(
