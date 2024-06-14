67a7ade4b949b8a930ed3e246beff1bb1c43d3eb
abhipray-cpu <dumkaabhipray@gmail.com>
2024-06-14 13:56:54 +0530

    imaages changes

--- src/components/home/ImageComp.vue
+++ src/components/home/ImageComp.vue
@@ -2,20 +2,16 @@
   <div class="group">
     <img
       src="https://firebasestorage.googleapis.com/v0/b/portfolio-b3f3a.appspot.com/o/Heaven.jpg?alt=media&token=d8c5fc24-fd9c-4b05-a079-1d4968e4a20e"
-      class="card big"
-    />
+      class="card big" />
     <img
       src="https://firebasestorage.googleapis.com/v0/b/portfolio-b3f3a.appspot.com/o/Bike.jpg?alt=media&token=c863399c-120b-4ce2-bcec-2ef6e0b76d53"
-      class="card big"
-    />
+      class="card big" />
     <img
-      src="https://firebasestorage.googleapis.com/v0/b/portfolio-b3f3a.appspot.com/o/WhatsApp%20Image%202024-06-07%20at%2012.04.35_2cf6a146.jpg?alt=media&token=bfa0d9d1-6532-42cd-b654-602cb5772d57"
-      class="card big"
-    />
+      src="https://firebasestorage.googleapis.com/v0/b/portfolio-b3f3a.appspot.com/o/images%2Fdp.jpg?alt=media&token=11c7e996-440d-4b57-a26c-269ac5892e8b"
+      class="card big" />
     <img
       src="https://firebasestorage.googleapis.com/v0/b/portfolio-b3f3a.appspot.com/o/tattoo.jpeg?alt=media&token=a076988e-1b35-468d-9243-8077eaf644d7"
-      class="card big"
-    />
+      class="card big" />
 
     <img src="" class="card small" />
     <img src="" class="card small" />
@@ -25,8 +21,7 @@
 </template>
 
 <style scoped>
-.group {
-}
+.group {}
 
 .card {
   border-radius: 16px;
